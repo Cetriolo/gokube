b27c87d5ea5ed50fcca3e0b1ffaac9b0c5be9690
Cetru <test@email.com>
2025-09-08 20:03:20 +0200

    changes

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,34 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"ride-sharing/shared/contracts"
 )
 
 func handleTripPreview(w http.ResponseWriter, r *http.Request) {
-
 	var reqBody previewTripRequest
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
 		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
 		return
 	}
-
 	defer r.Body.Close()
-
-	//validation
+	// validation
 	if reqBody.UserID == "" {
-		http.Error(w, "userID is required", http.StatusBadRequest)
+		http.Error(w, "user ID is required", http.StatusBadRequest)
 		return
 	}
-
-	response := contracts.APIResponse{Data: "ok"}
-	//TODO call trip service
-	err := writeJSON(w, http.StatusCreated, response)
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
+	// TODO: Call trip service
+	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
 
 	if err != nil {
-		http.Error(w, "Error response", http.StatusInternalServerError)
+		log.Print(err)
 		return
 	}
+	defer resp.Body.Close()
+	var respBody any
+	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
+		return
+	}
+	response := contracts.APIResponse{Data: respBody}
+	writeJSON(w, http.StatusCreated, response)
 
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"context"
+
 	"log"
+
 	"ride-sharing/services/trip-service/internal/domain"
+
+	"net/http"
+
+	h "ride-sharing/services/trip-service/internal/infrastructure/http"
+
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
+
 	"ride-sharing/services/trip-service/internal/service"
+
 	"time"
 )
 
@@ -13,17 +22,27 @@ func main() {
 	ctx := context.Background()
 	inmemRepo := repository.NewInmemRepository()
 	svc := service.NewService(inmemRepo)
+	mux := http.NewServeMux()
 	fare := &domain.RideFareModel{
 		UserID: "42",
 	}
+	httphandler := h.HttpHandler{Service: svc}
 	t, err := svc.CreateTrip(ctx, fare)
 	if err != nil {
 		log.Println(err)
-	}
-	log.Println(t.UserID)
 
-	//Keep the service running
+	}
+	mux.HandleFunc("POST /preview", httphandler.HandleTripPreview)
+	log.Println(t)
+	server := &http.Server{
+		Addr:    ":8083",
+		Handler: mux,
+	}
+	// keep the program running for now
 	for {
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Second)
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("HTTP server error: %v", err)
+		}
 	}
 }
